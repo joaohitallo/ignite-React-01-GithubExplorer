a407169d6f39a42702e3668d8ea80ecde7253b5e
joaohitallo <joaohitallo97@gmail.com>
2021-04-14 20:57:00 -0300

    Listando repositorios - aula 18

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -5,11 +5,6 @@ import { useState, useEffect } from "react";
 
 // https://api.github.com/orgs/rocketseat/repos
 
-const repository = {
-    name: 'unform',
-    description: 'Forms in React',
-    link: 'https://github.com/unform/unform'
-}
 
 export function RepositoryList(){
 
@@ -24,11 +19,13 @@ export function RepositoryList(){
     return (
         <section className="repository-list">
             <h1>Lista de repositorios </h1>
+
         <ul>
-            <RepositoryItem  repository={repository}/>
-            <RepositoryItem  repository={repository}/>
-            <RepositoryItem  repository={repository}/>
-            <RepositoryItem  repository={repository}/>
+            {repositories.map(repository =>{
+                return <RepositoryItem key={repository.name} repository={repository}/>
+            })}
+            
+
         </ul>
         </section>
     );
